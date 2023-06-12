ea40856b5bef767d445231cd442ade4241d47028
EliasCatalan95 <133457094+EliasCatalan95@users.noreply.github.com>
2023-06-11 21:58:22 -0500

    Motrar Registros Primera Fase

--- src/views/Registros.vue
+++ src/views/Registros.vue
@@ -1,15 +1,46 @@
 <template>
-  <ion-page >
- 
-      <div class="Nombre">
+      
+  <div >
+    <ul>
+      <li v-for="item in data" :key="item.id">
+        (Nombre:  {{ item.nombre }}) (ID: {{ item.id }})
+      </li>
+    </ul>
+  </div>
 
-        <ion-item>
-          <ion-title class="ion-text-center">Registros</ion-title>
-
-        </ion-item>
-      </div>
-  
-  </ion-page>
 </template>
 
 
+<script>
+import { ref, onMounted } from 'vue';
+import { getDatabase, ref as databaseRef, onValue } from 'firebase/database';
+
+export default {
+  setup() {
+    const database = getDatabase();
+    const data = ref([]);
+
+    onMounted(() => {
+      // Obtener los datos de Realtime Database
+      const dbRef = databaseRef(database, 'Nombres');
+      onValue(dbRef, (snapshot) => {
+        const dataSnapshot = snapshot.val();
+        const dataArray = [];
+        for (let key in dataSnapshot) {
+          const item = {
+            id: key,
+            nombre: dataSnapshot[key].nombre,
+           
+          };
+          dataArray.push(item);
+        }
+        data.value = dataArray;
+      });
+    });
+
+    return {
+      data
+    };
+  }
+};
+</script>
